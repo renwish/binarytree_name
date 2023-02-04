af8c3526e18cd8553e477275eb5818c771415a54
renwish <eugenebasinio@gmail.com>
2023-02-05 04:16:31 +0800

    Add project files.

---
+++ binarytreename.py
@@ -0,0 +1,66 @@
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def add_child(self, data):
+        if data == self.data:
+            return
+
+        if data < self.data:
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+
+        else:
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data:
+            if self.left:
+                return self.left.search(val)
+            else:
+                return False
+
+        if val > self.data:
+            if self.right:
+                return self.right.search(val)
+            else:
+                return False
+
+    def in_order_traversal(self):
+        elements = []
+
+        if self.left:
+            elements += self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements += self.right.in_order_traversal()
+
+            return elements
+
+def build_tree(elements):
+    print("Building the tree with these elements:", elements)
+    root = BinarySearchTreeNode(elements[0])
+
+    for i in range(1,len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
+if __name__ == '__main__':
+    letters = ["n","e","r","u","g","b","a","s","i","o"]
+    letter_tree = build_tree(letters)
+
+    print("is N a letter in my name? ", letter_tree.search("n"))
+    print("Does my name contain the letter Y? ", letter_tree.search("y"))
